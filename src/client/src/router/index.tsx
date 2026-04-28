30860397f829ec5e5818c623596c156d23a11ce4
Julien Mimouni <julienmimouni1@gmail.com>
2026-04-28 09:54:58 +0200

    feat(router) : implémentation de PrivateRoute pour sécuriser l'accès aux pages privées

--- src/client/src/router/index.tsx
+++ src/client/src/router/index.tsx
@@ -1,6 +1,7 @@
 import { createBrowserRouter } from 'react-router-dom'
 import RegisterPage from '../pages/auth/RegisterPage'
 import LoginPage from '../pages/auth/LoginPage'
+import PrivateRoute from '../components/PrivateRoute'
 
 export const router = createBrowserRouter([
 
@@ -13,10 +14,30 @@ export const router = createBrowserRouter([
 {
   path: '/login',
   element: <LoginPage />,
-}
+},
 
  // --- ROUTES PRIVÉES (Protégées) ---
 
-
+  {
+    // On met le PrivateRoute en parent
+    element: <PrivateRoute />, 
+    children: [
+      {
+        path: '/dashboard',
+        // element: <DashboardPage />, // À décommenter quand il sera créée
+        element: <div>Bienvenue sur ton Dashboard sécurisé !</div>,
+      },
+      {
+        path: '/parameters',
+        // element: <ParametersPage />, // À décommenter quand il sera créée
+        element: <div>Page des paramètres</div>,
+      }, 
+      {
+        path: '/mentions-legales',
+        // element: <MentionsLegalesPage />, // À décommenter quand il sera créée
+        element: <div>Page des mentions légales</div>,
+      }
+    ]
+  }
 
 ])
