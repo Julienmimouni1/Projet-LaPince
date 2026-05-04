c0fc44db59f7a72c21fe2109e0a1284ee534dcba
cathychereau <cathy.chereau@oclock.school>
2026-05-04 14:23:31 +0200

    fnally commit

--- src/api/controllers/category.controller.ts
+++ src/api/controllers/category.controller.ts
@@ -6,24 +6,10 @@ import { prisma } from "../lib/prisma.js";
 export async function getAllCategory(req:Request, res: Response ) {
 
   try {
-    // recupère le paramètre "default" dans l'URL (?default = true)
-    const isDefault = req.query.default === "true";
-    const id_user = req.query.id_user ? Number(req.query.id_user) : undefined;
 
-// personnalisation des catégories pour les utilisateurs
-    const categories = await prisma.category.findMany({
-    where:isDefault
-        ? { is_default: true }
-        : {
-             OR:  [
-              { is_default: true },
-              ...(id_user ? [{ id_user }] : []),
-            ],
-          },
-    });
-
-
-       //retourne la liste de catégories  
+    const categories = await prisma.category.findMany();
+   
+ //retourne la liste de catégories  
     return res.status(200).json(categories);
         
   } catch (error) {
@@ -77,82 +63,3 @@ try {
 }
 }
 
-export async function createCategory(req: Request, res: Response) {
-  const { name, color, icon, type, is_default, id_user } = req.body;
-
-  try {
-    const category = await prisma.category.create({
-      data: {
-        name,
-        color,
-        icon,
-        type,
-        is_default,
-        id_user, 
-      },
-    });
-
-    return res.status(201).json(category);
-  } catch (error) {
-    return res.status(500).json({
-      message: "Erreur création catégorie",
-    });
-  }
-}
-
-export async function updateCategory(req: Request, res: Response) {
-  const { id } = req.params;
-
-  try {
-    const updated = await prisma.category.update({
-      where: { id: Number(id) },
-      data: req.body, 
-    });
-
-    return res.json(updated);
-  } catch (error) {
-    return res.status(500).json({
-      message: "Erreur update catégorie",
-    });
-  }
-}
-
-export async function deleteCategory(req: Request, res: Response) {
-const { id } = req.params;
-
-try {
-    // 1. récupérer la catégorie en base
-    const category = await prisma.category.findUnique({
-    where: {
-        id: Number(id),
-    },
-    });
-
-    // 2. vérifier si elle existe
-    if (!category) {
-    return res.status(404).json({
-        message: "Catégorie introuvable",
-    });
-    }
-
-    // 3. bloquer si c'est une catégorie par défaut
-    if (category.is_default === true) {
-    return res.status(403).json({
-        message: "Suppression non autorisée",
-    });
-    }
-
-    // 4. supprimer la catégorie
-    await prisma.category.delete({
-    where: {
-        id: Number(id),
-    },
-    });
-
-    return res.sendStatus(204);
-  } catch (error) {
-    return res.status(500).json({
-      message: "Erreur lors de la suppression de la catégorie",
-    });
-  }
-}
--- src/api/routes/category.routes.ts
+++ src/api/routes/category.routes.ts
@@ -1,10 +1,8 @@
 import { Router } from "express"
 
-import { getAllCategory } from "../controllers/category.controller.ts"
-import { getOneCategory } from "../controllers/category.controller.ts";
-import { createCategory } from "../controllers/category.controller.ts";
-import { updateCategory } from "../controllers/category.controller.ts";
-import { deleteCategory } from "../controllers/category.controller.ts";
+import { getAllCategory, getOneCategory } from "../controllers/category.controller.ts"
+
+
 //import { authMiddleware } from "../middlewares/access.controller.middleware.ts";
 
 
@@ -13,10 +11,6 @@ const router = Router();
 router.get("/",  getAllCategory);
 router.get("/:id",  getOneCategory);
 
-router.post("/", createCategory);
 
-router.patch("/:id",  updateCategory)
-
-router.delete("/:id",  deleteCategory)
 
 export default router;
